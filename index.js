8be7a72c0a23500087cc24f456a8381af06f0eb9
alsafa012 <alsafa012@gmail.com>
2023-11-24 02:39:02 +0600

    users api created and post method applied for store user info in database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
@@ -7,6 +8,68 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pz6rkt0.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+     const userCollection = client.db("contestDB").collection("users");
+
+     // user api
+
+
+
+     app.post("/users", async (req, res) => {
+          const user = req.body;
+          const query = { email: user.email };
+          console.log('Query', query);
+          const existingUser = await userCollection.findOne(query);
+          if (existingUser) {
+               return res.send({ message: "user already exists" });
+          }
+          const result = await userCollection.insertOne(user);
+          res.send(result);
+     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+//     await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 
 
